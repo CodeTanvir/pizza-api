ceb1e535106209aa12f63bc62142bfd42043a3c4
Tanvir Hossain <info.tanvirx@gmail.com>
2024-06-30 15:41:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin:'http://localhost:5173'
+}))
 dotenv.config();
 
 const _name = process.env.name;
